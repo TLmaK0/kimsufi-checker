4fc6ea1c39533b77f90661fa18079431e75c2ad9
Hugo Freire <hugofreiregil@gmail.com>
2022-02-04 17:10:39 +0100

    Add country code

--- check.js
+++ check.js
@@ -4,15 +4,17 @@ const opn = require('opn');
 
 var requests = 0;
 
-if (!process.argv[2] || !process.argv[3]) {
-  console.error('Usage: node check.js <seconds_wait> <serverid> <serverid> ....');
-  console.info('\nnode check.js 10 1801sk13 1801sk12');
-  console.info('Check every 10 seconds for server 1801sk13 and 1801sk12');
+if (!process.argv[4]) {
+  console.error('Usage: node check.js <secondsWait> <countryId> <hardwardId> <hardwardId> ....');
+  console.info('\nnode check.js 10 ES 1801sk13 1801sk12');
+  console.info('Check every 10 seconds for server 1801sk13 and 1801sk12 on country subsidiary company ES');
+  printCountries();
   return;
 }
 
-var time = process.argv[2]; //in seconds
-var servers = process.argv.slice(3);
+const time = process.argv[2]; //in seconds
+const country = process.argv[3];
+const servers = process.argv.slice(4);
 
 call();
 
@@ -20,7 +22,8 @@ function call(){
   requests++;
   process.stdout.clearLine();
   process.stdout.write(`Requests: ${requests}. Checking for ${servers.join(', ')} ...\r`);
-  request('https://www.ovh.com/engine/api/dedicated/server/availabilities?country=es', { json: true }, (err, res, body) => {
+  request(`https://www.ovh.com/engine/api/dedicated/server/availabilities?country=${country}`, { json: true }, (err, _res, body) => {
+    if (err) console.error(err);
     const nodes = _.filter(body, (item)=>{
       return servers.includes(item.hardware);
     });
@@ -31,7 +34,7 @@ function call(){
       }).length > 0
     });
     if (availables[0]) {
-      const url = `https://www.kimsufi.com/es/pedido/kemsirve.xml?reference=${availables[0][0]}`;
+      const url = `https://www.kimsufi.com/en/order/kimsufi.xml?reference=${availables[0][0]}`;
       process.stdout.write(`Available. Opening: ${url}\r`);
       opn(url);
       return;
@@ -41,3 +44,10 @@ function call(){
     setTimeout(call, time * 1000);
   });
 };
+
+function printCountries() {
+  request(`https://api.ovh.com/1.0/dedicated/server.json`, { json: true }, (_err, _res, body) => {
+    const countries = body.models['nichandle.OvhSubsidiaryEnum'].enum.join(', ');
+    process.stdout.write(`Available countries: ${countries}\n`);
+  });
+}
